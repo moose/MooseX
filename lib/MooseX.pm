cb48938312b98a4b56ca4e4f0835b752a3d80fab
Karen Etheridge <ether@cpan.org>
2018-12-07 19:28:25 -0800

    vim modeline must appear in the first five lines

--- lib/MooseX.pm
+++ lib/MooseX.pm
@@ -1,9 +1,9 @@
 use strict;
 use warnings;
 package MooseX;
+# vim: set ts=8 sts=4 sw=4 tw=115 et :
 # ABSTRACT: Document the MooseX namespace
 # KEYWORDS: Moose documentation namespace nomenclature best-practices extensions ecosystem
-# vim: set ts=8 sts=4 sw=4 tw=115 et :
 
 our $VERSION = '0.003';
 
